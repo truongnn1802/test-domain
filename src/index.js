549ec9b31f6f780f014354e104ee62723602ebd5
truongnn1802 <truongnn2081@gmail.com>
2023-04-03 12:30:07 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const domain = window.location.hostname;
+const domain = window.location;
 console.log(domain);
 root.render(
   <React.StrictMode>
