1c8c1beaa019bbf99d9d913f9b01276a811a0f21
truongnn1802 <truongnn2081@gmail.com>
2023-04-04 09:55:34 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import "./App.css";
 function App() {
   const domain = window.location;
   console.log(domain.hostname);
-  console.log(domain.hostname.split('.')[0]);
+  console.log(domain.hostname.split(".")[0]);
 
-  
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const templates = {
+    "tieu-hoc": <p>Tiểu học</p>,
+    "mam-non": <p>Tiểu học</p>,
+    thcs: <p>Tiểu học</p>,
+    pgd: <p>Tiểu học</p>,
+    default: <p>Test</p>,
+  };
+
+  console.log(templates[domain.hostname]);
+  const renderLayout = () => {
+    return templates[domain.hostname] || templates['default'];
+  };
+  return <div className="App">{renderLayout()}</div>;
 }
 
 export default App;
